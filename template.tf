06c9770e00559dbafdefc50c11ddbe96e2e69aa4
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-30 10:31:39 +0200

    Create template.tf

---
+++ template.tf
@@ -0,0 +1,87 @@
+provider "azurerm" {
+  
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
+  features {}
+}
+#File =template.tf
+resource "azurerm_resource_group" "main" {
+  name     = "${var.resourceGroup}"
+  location = "${var.region}"
+  tags = "${local.common_tags}"
+}
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["${var.address_space}"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  #tags = "${jsondecode(var.customtags)}"
+  tags = "${local.common_tags}"
+}
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "${var.subnet_prefix}"
+}
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  ip_configuration {
+    name                          = "${var.prefix}-ipconfiguration"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.test.id}"
+  }
+  tags = "${local.common_tags}"
+}
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-azure-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.vmSize}"
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+  storage_image_reference {
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
+  }
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.hostname}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+ }
+  tags = "${local.common_tags}"
+  
+}
+resource "azurerm_public_ip" "test" {
+  name                = "${var.prefix}-PublicIp"
+  location            = "${var.region}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  allocation_method   = "Dynamic"
+  tags = "${local.common_tags}"
+  }
+  locals {
+  # Commmon tags to be assigned to all resources
+  common_tags = "${map(
+   " BusinessService", "${var.business_service}",
+   " CostCenter", "${var.cost_center}"
+ )}"
+}
